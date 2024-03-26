cae390f3147e978828ee23c52bd9390c5282d5f1
LuizCorsini <luizfernandooffice@gmail.com>
2024-03-26 16:20:22 -0300

    SIC V.0.1.3 { Configuração de Autenticação tela de Login }

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,35 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './pages/Home';
 import Reports from './pages/Reports';
-import { createGlobalStyle } from 'styled-components';
+//import { createGlobalStyle } from 'styled-components';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ThemeProvider from 'react-bootstrap/ThemeProvider'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import SignIn from './pages/Signin';
 
 
-const GlobalStyle = createGlobalStyle`
-    body {
-      margin: 0;
-      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-        sans-serif;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-      background-color: #F9F9F9;
-    }
+// const GlobalStyle = createGlobalStyle`
+//     body {
+//       margin: 0;
+//       font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+//         'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+//         sans-serif;
+//       -webkit-font-smoothing: antialiased;
+//       -moz-osx-font-smoothing: grayscale;
+//       background-color: #F9F9F9;
+//     }
 
-    code {
-      font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-        monospace;
-    }
+//     code {
+//       font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+//         monospace;
+//     }
 
-`
+// `
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
+    <ThemeProvider>
       <BrowserRouter>
       <Routes>
         <Route path='/home' element={<Home />} />
@@ -38,6 +39,7 @@ root.render(
         <Route path='/sign-in' element={<SignIn />} />
       </Routes>
       </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
--- frontend/src/pages/Signin/index.js
+++ frontend/src/pages/Signin/index.js
@@ -1,22 +1,25 @@
 import React from "react";
 import styled from "styled-components";
-import Background from "../../imagens/background.jpg"
+import Background from "../../imagens/background2.png"
 import { Row, Col, Form, Button } from "react-bootstrap";
+import { faRightToBracket, faUserLock } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ColLeft = styled(Col)`
     background-image: url(${Background});
     background-position: center;
     background-size: cover;
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     height: 100%; /* Garante que a div ocupe toda a altura */
 `;
 
 const StyledH1 = styled.h1`
-    font-weight: 600;
+    font-weight: 700;
     color: hsl(24, 70%, 50%);
-    font-size: 50px;
+    font-size: 70px;
 `
 
 const ColRigth= styled(Col)`
@@ -31,37 +34,72 @@ const StyleRow = styled(Row)`
     height: 100vh;
 `;
 
+const StyleStrong = styled.strong`
+    color: rgb(91, 0, 177);
+`;
+
+const StyledfaRightToBracket = styled(FontAwesomeIcon)`
+    margin-right: 10px
+`;
+
+
+const StyledFormControl= styled(Form.Control)`
+    border-left: none;
+    border-right: none;
+    border-top: none;
+    border-radius: 0;
+    caret -color: rgb(255, 145, 0);
+`;
+
+const StyledButton = styled(Button)`
+    background: hsl(24, 70%, 50%);
+    border: 0;
+    &:hover{
+        background: rgb(91, 0, 177);
+    }
+    &:active{
+        background: false;
+        box-shadow: 0 1px #666;
+        transform: translateY(1px)
+    }
+`;
 
 function SignIn() {
     return (
         <StyleRow>
-            <ColLeft xs={7}>
-                    <Col xs={7}>
-                        <StyledH1>Facilitando sua Forma de Gerar Relatórios Contratuais</StyledH1>
-                        <p>Subtítulo</p>
-                    </Col>
+            <ColLeft 
+                xs={7}>
+                    <StyledH1 
+                    as={Col} 
+                    md={9}>Compromissos Contratuais em <StyleStrong>um</StyleStrong> Toque</StyledH1>
             </ColLeft>
-            <ColRigth>             
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
-                                <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                                </Form.Text>
-                            </Form.Group>
 
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                                <Form.Check type="checkbox" label="Check me out" />
-                            </Form.Group>
-                            <Button variant="primary" type="submit">
-                                Submit
-                            </Button>
-                    </Form>
+
+            <ColRigth md={5}>
+
+                <Form as={Col} md={8}>
+                <Row className="justify-content-md-center">
+                    <FontAwesomeIcon style={{color:'hsl(24, 70%, 50%)'}} icon={faUserLock} className="mb-3" as={Col} size="2x" md="auto"  />
+                </Row>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>E-mail</Form.Label>
+                        <StyledFormControl type="email" placeholder="Digite seu e-mail" />
+                        <Form.Text className="text-muted">
+                        Não compartilhamos seus dados de e-mail com terceiros.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Senha</Form.Label>
+                        <StyledFormControl type="password" placeholder="Digite sua senha" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <StyledButton variant="primary" type="submit">
+                        <StyledfaRightToBracket active='false' icon={faRightToBracket}></StyledfaRightToBracket><span>Entrar</span>
+                    </StyledButton>
+                </Form>
             </ColRigth>            
         </StyleRow>
     );
